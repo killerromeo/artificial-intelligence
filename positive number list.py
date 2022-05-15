6d67bc011d718253e5eb26c04786f436e644cef2
killerromeo <93936624+killerromeo@users.noreply.github.com>
2022-05-15 12:21:56 +0530

    Create positive number list.py

---
+++ positive number list.py
@@ -0,0 +1,9 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+for x in list1:
+          if x>0:
+               print(x,end=" ")
+print("\n")
+for y in list2:
+     if y>0:
+          print(y,end=" ")
